eae369a4bc9c9f33e7205d5b7f6a270cafdf0335
Phil Businger <pbusinger@deloitte.com>
2024-05-27 06:01:19 -0400

    get player and computer choices

---
+++ rps.js
@@ -0,0 +1,28 @@
+console.log("Hello World")
+
+function getRandomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)    
+}
+
+function getComputerChoice() {
+    var computerChoice =  getRandomInt(1,3)
+    switch (computerChoice) {
+        case 1: return "rock"
+        case 2: return "paper"
+        case 3: return "scissors"
+    }
+}
+
+function getPlayerChoice() {
+    var playerChoice = prompt("Enter a choice: ")
+    var validChoices = ["rock", "paper", "scissors"]
+    while (!(validChoices.includes(playerChoice.toLowerCase()))) {
+        playerChoice = prompt("That is not a valid response. Please try again: ")
+    }
+    return playerChoice
+}
+
+var computerChoice = getComputerChoice();
+console.log(computerChoice)
+var playerChoice = getPlayerChoice()
+console.log(playerChoice)
