6f75850636499b11e28f0465f2ad8f485c4e424b
Phil Businger <pbusinger@deloitte.com>
2024-05-27 06:25:19 -0400

    paly a round and keep score

--- rps.js
+++ rps.js
@@ -4,25 +4,61 @@ function getRandomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min)    
 }
 
-function getComputerChoice() {
-    var computerChoice =  getRandomInt(1,3)
-    switch (computerChoice) {
+function rpsIntToStr(rps) {
+    switch(rps) {
         case 1: return "rock"
         case 2: return "paper"
         case 3: return "scissors"
     }
 }
 
+function rpsStrToInt(rps) {
+    switch(rps) {
+        case "rock": return 1
+        case "paper": return 2
+        case "scissors": return 3
+    }
+}
+
+function getComputerChoice() {
+    var computerChoice =  getRandomInt(1,3)
+    return rpsIntToStr(computerChoice)
+}
+
 function getPlayerChoice() {
-    var playerChoice = prompt("Enter a choice: ")
+    var playerChoice = prompt("Enter a choice: ").toLowerCase()
     var validChoices = ["rock", "paper", "scissors"]
-    while (!(validChoices.includes(playerChoice.toLowerCase()))) {
+    while (!(validChoices.includes(playerChoice))) {
         playerChoice = prompt("That is not a valid response. Please try again: ")
     }
     return playerChoice.toLowerCase()
 }
 
-var computerChoice = getComputerChoice();
-console.log(computerChoice)
+function playRound(playerChoice, computerChoice) {
+    let playerChoiceInt = rpsStrToInt(playerChoice)
+    let computerChoiceInt = rpsStrToInt(computerChoice)
+    if (playerChoice == computerChoice)
+        return "It's a tie!"
+    else if ((playerChoiceInt == 0 && computerChoiceInt == 3) || (playerChoiceInt > computerChoiceInt)) {
+        playerScore++
+        console.log(playerScore)
+        return "Player wins!"
+    }
+    else {
+        computerScore++
+        console.log(computerScore)
+        return "Computer wins!"
+    }
+}
+
+
+var playerScore = 0
+var computerScore = 0
+var computerChoice = getComputerChoice()
 var playerChoice = getPlayerChoice()
+
+console.log(computerChoice)
 console.log(playerChoice)
+console.log(playRound(playerChoice, computerChoice))
+console.log(playerScore)
+console.log(computerScore)
